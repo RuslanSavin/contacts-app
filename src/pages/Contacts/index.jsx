11a014ddf9b42569440dcb5dde65f43eb241418c
RuslanSavin <savinruslan67@gmail.com>
2022-05-09 00:41:27 +0300

    create useContacts, add MUI

--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -1,3 +1,15 @@
+import { useContacts } from "../../hooks/useContacts";
+
 export const Contacts = () => {
-  return <div>Contacts</div>;
+  const contacts = useContacts();
+
+  if (contacts.isLoading) {
+    return <div>...loading</div>;
+  }
+
+  if (contacts.isError) {
+    return <div>...error</div>;
+  }
+
+  return <div>{contacts.data[0].email}</div>;
 };
